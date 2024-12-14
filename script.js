828ffa302c48ed5b7521539f46a28c13b56ac898
MennaMagdie <mennamagdyx@gmail.com>
2024-12-14 10:03:45 +0200

    FInished update feature

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ const selectedForm = document.forms.taskForm;
 const taskListContainer = document.querySelector("#taskListContainer");
 
 const tasks = JSON.parse(localStorage.getItem("tasks"));
-let taskList = tasks || []; //stores all task values read from DOM
-
+let taskList = tasks || []; //all task values read from DOM
 
 selectedForm.addEventListener("submit", (e) => {
-    e.preventDefault(); //tried removing
+    e.preventDefault();
     newTaskHandler();
 });
 
-updateDOM(); //why standalone?
+updateDOM();
 
 function newTaskHandler() {
-    const taskContent = selectedForm["name-task"].value; //value di eh eldtype bta3etha?
+    const taskContent = selectedForm["name-task"].value;
     taskList.push(taskContent);
     update();
 
-    selectedForm["name-task"].value = ""; //tried removing
+    selectedForm["name-task"].value = ""; 
 }
 
 function update() {
@@ -38,30 +37,35 @@ function updateDOM() {
 }
 
 function renderTask(task) {
-    const newElement = document.createElement("p");
-    newElement.innerText = task;
-    newElement.className = "task-item";
 
+    const newElement = document.createElement("div");
+    newElement.className = "task-item-container";
 
+    const taskText = document.createElement("p");
+    taskText.innerText = task;
+    taskText.className = "task-item";
+    newElement.append(taskText);
+
+    // 1. Update
     const updateButton = document.createElement("button");
     updateButton.innerText = "Update";
     updateButton.className = "update-task";
-    updateButton.addEventListener("click", (e) => {
-        updateTaskValue(task);
+    
+    updateButton.addEventListener("click", () => {
+        updateTask(newElement, task);
     });
     newElement.append(updateButton);
 
-
+    // 2. Delete
     const deleteButton = document.createElement("button");
-    deleteButton.innerText = "Delete";
+    deleteButton.innerText = "Done";
     deleteButton.className = "delete-task";
-    newElement.append(deleteButton);
-    deleteButton.addEventListener("click", (e) => {
+    deleteButton.addEventListener("click", () => {
         deleteTask(task);
     });
+    newElement.append(deleteButton);
 
     taskListContainer.prepend(newElement);
-    
 }
 
 function deleteTask(task) {
@@ -69,7 +73,47 @@ function deleteTask(task) {
     update();
 }
 
-function updateTaskValue(task) {
-    taskList = taskList.filter((x) => x !== task);
-    newTaskHandler();
+function updateTask(newElement, task) {
+
+    const container = document.createElement("div");
+    container.className = "update-container";
+
+    const newInput = document.createElement("input");
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("placeholder", "Enter Updated Task");
+    // newInput.setAttribute("required", true);
+    newInput.className = "update-value";
+    newInput.value = task; //prefill
+    container.appendChild(newInput);
+
+    const updatedButton = document.createElement("button");
+    updatedButton.innerText = "Save";
+    updatedButton.className = "save-task";
+    container.appendChild(updatedButton);
+
+    newElement.after(container);
+
+    updatedButton.addEventListener("click", () => {
+        
+        const updatedValue = newInput.value;
+        updateTaskList(task, updatedValue);
+
+        update();
+        container.remove();
+        }
+    );
+
+}
+
+function updateTaskList(oldTask, updatedValue) {
+
+    if (updatedValue !== "") {
+        
+        const index = taskList.findIndex((task)=> task === oldTask);
+        if(index !== -1) {
+            taskList[index] = updatedValue
+        }
+    
+}
+
 }
